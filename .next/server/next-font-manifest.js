9a0d3e435a0754f5ed934bced905b2b3f28f1e14
Kuldeep Khalotiya <kuldeepkhalotiya5@gmail.com>
2025-11-18 16:55:49 +0530

    bug fix

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/kuldeepkhalotiya/Documents/arturo-portfolio/app/layout\":[\"static/media/36966cca54120369-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
